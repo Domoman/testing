4628abae1218b705a26704e71761fb77835d6e31
Domoman <huangkaiyang@gmail.com>
2018-11-16 08:23:12 +0800

    Add files via upload

---
+++ Payment.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Stripe payment</title>
+
+	<form action="your-server-side-code" method="POST">
+  <script
+    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+    data-key="pk_test_gwRoWLISPVXvGxpnlhOf1Cg6"
+    data-amount="999"
+    data-name="Demo Site"
+    data-description="Example charge"
+    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+    data-locale="auto">
+  </script>
+</form>
+	
+</head>
+<body>
+
+</body>
+</html>
