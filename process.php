7b7d29acdf1feb2fb2dd4843c4f45598a447ff28
Domoman <huangkaiyang@gmail.com>
2018-11-16 08:37:27 +0800

    Create process.php

---
+++ process.php
@@ -0,0 +1,20 @@
+<?php
+
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+\Stripe\Stripe::setApiKey("sk_test_eaS2OL9Pp9LCEp0Zup9Ru2uZ");
+
+// Token is created using Checkout or Elements!
+// Get the payment token ID submitted by the form:
+$token = $_POST['stripeToken'];
+
+$charge = \Stripe\Charge::create([
+    'amount' => 999,
+    'currency' => 'usd',
+    'description' => 'Example charge',
+    'source' => $token,
+]);
+
+
+
+?>
